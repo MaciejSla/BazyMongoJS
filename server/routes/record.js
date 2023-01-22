c2af6470f282cb7ead7ba330d563ea16abb7660a
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-01-22 16:39:09 +0100

    Finished

--- server/routes/record.js
+++ server/routes/record.js
@@ -3,77 +3,104 @@ const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
-recordRoutes.route("/products").get(function (req, res) {
-  let db_connect = dbo.getDb();
-  db_connect
+recordRoutes.route("/products").get(async function (req, response) {
+  const db_connect = dbo.getDb();
+
+  let sortBy = req.query.sortBy;
+  let lessThan = req.query.lessThan;
+  let moreThan = req.query.moreThan;
+  let filterBy = req.query.filterBy;
+
+  sortBy = sortBy === undefined ? "name" : sortBy;
+  filterBy = filterBy === undefined ? "price" : filterBy;
+  lessThan = lessThan === undefined ? 1000000 : parseInt(lessThan);
+  moreThan = moreThan === undefined ? 0 : parseInt(moreThan);
+
+  await db_connect
     .collection("products")
-    .find({})
+    .find({ [filterBy]: { $gt: moreThan, $lt: lessThan } })
+    .sort({ [sortBy]: 1 })
     .toArray(function (err, result) {
       if (err) throw err;
-      res.json(result);
+      response.status(200).json(result);
     });
 });
 
-// recordRoutes.route("/record/:id").get(function (req, res) {
-//   let db_connect = dbo.getDb();
-//   let myquery = { _id: ObjectId(req.params.id) };
-//   db_connect.collection("products").findOne(myquery, function (err, result) {
-//     if (err) throw err;
-//     res.json(result);
-//   });
-// });
-
-recordRoutes.route("/products").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myobj = {
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price,
-    amount: req.body.amount,
-    unit: req.body.unit,
-  };
-  db_connect
+recordRoutes.route("/products").post(async function (req, response) {
+  const db_connect = dbo.getDb();
+  await db_connect
     .collection("products")
     .findOne({ name: req.body.name }, function (err, res) {
       if (err) throw err;
       if (res == null) {
-        db_connect.collection("products").insertOne(myobj, function (err, res) {
-          if (err) throw err;
-          response.json(res);
-        });
+        db_connect
+          .collection("products")
+          .insertOne(req.body, function (err, res) {
+            if (err) throw err;
+            response.status(201).json(res);
+          });
       } else {
-        response.send("Produkt już istnieje.");
+        response.status(400).send("Produkt już istnieje.");
       }
     });
 });
 
-recordRoutes.route("/update/:id").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  let newValues = {
-    $set: {
-      name: req.body.name,
-      position: req.body.position,
-      level: req.body.level,
-    },
-  };
-  db_connect
+recordRoutes.route("/products/:id").put(async function (req, response) {
+  const db_connect = dbo.getDb();
+  const myquery = { _id: ObjectId(req.params.id) };
+  await db_connect
     .collection("products")
-    .updateOne(myquery, newValues, function (err, res) {
+    .updateOne(myquery, req.body, function (err, res) {
       if (err) throw err;
       console.log("1 document updated successfully");
-      response.json(res);
+      response.status(201).json(res);
     });
 });
 
-recordRoutes.route("/:id").delete(function (req, res) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("products").deleteOne(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log(`1 document deleted: ${req.params.id}`);
-    res.json(obj);
-  });
+recordRoutes.route("/products/:id").delete(async function (req, response) {
+  const db_connect = dbo.getDb();
+  if (req.params.id.length != 24) {
+    response.status(404).send("Produkt nie istnieje.");
+  } else {
+    const myquery = { _id: ObjectId(req.params.id) };
+
+    await db_connect
+      .collection("products")
+      .findOne(myquery, function (err, res) {
+        if (err) throw err;
+        if (res == null) {
+          response.status(404).send("Produkt nie istnieje.");
+        } else {
+          db_connect
+            .collection("products")
+            .deleteOne(myquery, function (err, obj) {
+              if (err) throw err;
+              console.log(`1 document deleted: ${req.params.id}`);
+              response.status(200).json(obj);
+            });
+        }
+      });
+  }
+});
+
+recordRoutes.route("/raport").get(async function (req, response) {
+  const db_connect = dbo.getDb();
+  const aggregate = db_connect.collection("products").aggregate([
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        amount: 1,
+        price: 1,
+        total: { $multiply: ["$price", "$amount"] },
+      },
+    },
+  ]);
+  let arr = [];
+  for await (const elem of aggregate) {
+    arr = [...arr, elem];
+  }
+  response.status(200).json(arr);
 });
 
 module.exports = recordRoutes;
